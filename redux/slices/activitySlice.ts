f02ce2071ba10f55c1a9cd649e3955b6b4c1833f
ccook0194 <ccook.dev.0011@gmail.com>
2025-02-14 13:12:09 -0500

    .

--- redux/slices/activitySlice.ts
+++ redux/slices/activitySlice.ts
@@ -9,7 +9,7 @@ export const fetchAgentActivity = createAsyncThunk("data/fetchAgentActivity", as
     },
     body: JSON.stringify({ 
       date: moment(new Date(2025, 1, 1/*(date.getDate() - 1) % 7 + 1*/)).format('YYYY-MM-DD'),
-      time: `${date.getHours()}:${date.getMinutes()}`
+      time: moment(date).format("HH:mm")
     }),
   })
 
